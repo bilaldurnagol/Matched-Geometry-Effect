b443576cebcdbbfe9fc28f005abb39afb6f3df5b
Bilal Durnagöl <bilal.durnagol.tt@gmail.com>
2022-09-22 14:47:58 +0300

    Initial Commit

---
+++ Matched Geometry Effect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matched Geometry Effect
+//
+//  Created by Bilal Durnagöl on 22.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matched Geometry Effect/Matched_Geometry_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_Geometry_EffectApp.swift
+//  Matched Geometry Effect
+//
+//  Created by Bilal Durnagöl on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Matched_Geometry_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
