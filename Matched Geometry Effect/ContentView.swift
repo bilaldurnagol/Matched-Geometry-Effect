48ba001cbf46dd044f6b4896fd93919407060d15
Bilal Durnagöl <bilal.durnagol.tt@gmail.com>
2022-09-22 18:05:26 +0300

    added matched geometry effect

--- Matched Geometry Effect/ContentView.swift
+++ Matched Geometry Effect/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    @State private var isProfileViewExpanded = true
+    @Namespace private var profileAnimation
+    @Namespace private var profileImageID
+    @Namespace private var profileNameID
+    @Namespace private var profileJobID
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if isProfileViewExpanded {
+                expandedProfileView
+            } else {
+                collapsedProfileView
+            }
+            
+            videoListView
+            
+        }
     }
 }
 
@@ -18,4 +35,89 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension ContentView {
+    
+    
+    var collapsedProfileView: some View {
+        HStack {
+            profileImage
+                .matchedGeometryEffect(id: profileImageID, in: profileAnimation)
+                .frame(width: 60, height: 60)
+            
+            VStack(alignment: .leading) {
+                
+                Text("Bilal Durnagöl")
+                    .matchedGeometryEffect(id: profileNameID, in: profileAnimation)
+                    .font(.title.bold())
+                
+                Text("iOS Developer")
+                    .matchedGeometryEffect(id: profileJobID, in: profileAnimation)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+        }
+        .padding()
+    }
+    
+    var expandedProfileView: some View {
+        VStack {
+            profileImage
+                .matchedGeometryEffect(id: profileImageID, in: profileAnimation)
+                .frame(width: 130, height: 130)
+            
+            VStack {
+                Text("Bilal Durnagöl")
+                    .matchedGeometryEffect(id: profileNameID, in: profileAnimation)
+                    .font(.title.bold())
+                
+                Text("iOS Developer")
+                    .matchedGeometryEffect(id: profileJobID, in: profileAnimation)
+                    .foregroundColor(.pink)
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+                    .padding()
+            }
+        }
+    }
+    
+    var videoListView: some View {
+        ScrollViewReader { proxy in
+            List {
+                ForEach(0..<50, id: \.self) { i in
+                    Color.gray
+                        .opacity(0.7)
+                        .frame(height: 150)
+                        .cornerRadius(10)
+                        .overlay {
+                            Circle()
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.pink)
+                                .overlay {
+                                    Image(systemName: "play.fill")
+                                        .foregroundColor(.white)
+                                }
+                        }
+                }
+            }
+            .simultaneousGesture(DragGesture().onChanged({ value in
+                withAnimation {
+                    isProfileViewExpanded = value.translation.height > 0
+                }
+            }))
+            .onAppear {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            }
+        }
+    }
+    
+    var profileImage: some View {
+        Image("profile_image")
+            .resizable()
+            .scaledToFill()
+            .clipShape(Circle())
+    }
+    
 }
